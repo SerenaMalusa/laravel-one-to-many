f14109fbb22f1e074ff9a09395d8df7a5f5eb13f
SerenaMalusa <sere.malu@gmail.com>
2024-04-07 17:31:38 +0200

    feat: add title to form view

--- resources/views/admin/types/form.blade.php
+++ resources/views/admin/types/form.blade.php
@@ -1,9 +1,22 @@
 @extends('layouts.app')
 
-@section('title', $title = "Register new type")
+@section('title') 
+    @if(isset($type->id))
+        {{ $title = "Modify " . $type->name }}
+    @else
+        {{ $title = 'Create new type'}}
+    @endif
+
+@endsection
 
 @section('content')
 
-    <h1>Ciao</h1>
+    <section>
+        <div class="container py-4">
+            <h1>{{ $title }}</h1>
+        </div>
+    </section>    
+
+    
 
 @endsection
